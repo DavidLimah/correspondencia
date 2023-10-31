6d925cd14ad8639c58f87810cbf0450436d67126
David Lima <david.limah@gmail.com>
2023-10-31 10:57:38 -0400

    Se agregan pruebas unitarias Form registro, Page View

--- users/tests.py
+++ users/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 # Prueba unitaria Users
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
 
 class CustomUserTests(TestCase):
@@ -19,8 +20,7 @@ class CustomUserTests(TestCase):
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
 
-    
-    # Prueba creación de super usuario
+    # Prueba unitaria creación de super usuario
     def test_create_superuser(self):
         User = get_user_model()
         admin_user = User.objects.create_superuser(
@@ -32,3 +32,26 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+    
+    # Prueba unitaria registro de usuario
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+    
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(
+            self.response, 'Error')
+    
+    def test_signup_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+    
+    def test_signup_view(self):
+        view = resolve('/accounts/signup/')
+        self.assertEqual(
+            view.func.__name__,
+            SignupPageView.as_view().__name__)
