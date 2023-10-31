3457e0a64b69f4ce1a1250472d60a46b1942797c
David Lima <david.limah@gmail.com>
2023-10-31 09:50:40 -0400

    Se corrige Error de login

--- app_correspondencia/settings.py
+++ app_correspondencia/settings.py
@@ -129,5 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
-LOGIN_REDIRECT = 'home'
+LOGIN_REDIRECT_URL = 'home'
+
 LOGOUT_REDIRECT_URL = 'home'
