4ab40f4c3fa9dc713d53c693a27f612842379722
David Lima <david.limah@gmail.com>
2023-11-08 12:02:59 -0400

    Se reestructura proyecto para corregir error de sesiones de usuario

--- users/models.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-# Módulo USERS
-class CustomUser(AbstractUser):
-    oficina = models.CharField(max_length=200)
-    cargo = models.CharField(max_length=200)
-
