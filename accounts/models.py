0314c921a83c47ac150d999286ae91df4ee1d391
David Lima <david.limah@gmail.com>
2023-11-09 22:59:10 -0400

    Se continua trabajando en modelo Accounts para sesiones de usuario

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,28 @@ from django.contrib.auth.models import User
 
 # 
 
+class Oficina(models.Model):
+	nombre_oficina = models.CharField(max_length=150)
+
+	class Meta:
+		verbose_name = 'Oficina'
+		verbose_name_plurak = 'Oficina'
+
+	def __str__(self):
+		return self.nombre_oficina
+
+class Cargo(models.Model):
+	nombre_cargo = models.CharField(max_length=150)
+
+	class Meta:
+		verbose_name = 'Oficina'
+		verbose_name_plurak = 'Oficina'
+		
+	def __str__(self):
+		return self.nombre_cargo
+
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 	image = models.ImageField(default='users/image_user.png',upload_to='users/')
-	location = models.CharField(max_length=80, null=True, blank=True)
+	oficina = models.OneToOneField(Oficina, on_delete=models.CASCADE, related_name='oficina')
+	cargo = models.OneToOneField(Cargo, on_delete=models.CASCADE, related_name='cargo')
