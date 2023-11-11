4945236b869ac641df435bc33d55d3c3855faebf
David Lima <david.limah@gmail.com>
2023-11-10 22:20:35 -0400

    Módulo Accounts aún con errores. No reconoce sesiones

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models.signals import post_save
 # 
 
 class Oficina(models.Model):
@@ -8,7 +8,7 @@ class Oficina(models.Model):
 
 	class Meta:
 		verbose_name = 'Oficina'
-		verbose_name_plurak = 'Oficina'
+		verbose_name_plural = 'Oficina'
 
 	def __str__(self):
 		return self.nombre_oficina
@@ -18,7 +18,7 @@ class Cargo(models.Model):
 
 	class Meta:
 		verbose_name = 'Oficina'
-		verbose_name_plurak = 'Oficina'
+		verbose_name_plural = 'Oficina'
 		
 	def __str__(self):
 		return self.nombre_cargo
@@ -28,3 +28,18 @@ class Profile(models.Model):
 	image = models.ImageField(default='users/image_user.png',upload_to='users/')
 	oficina = models.OneToOneField(Oficina, on_delete=models.CASCADE, related_name='oficina')
 	cargo = models.OneToOneField(Cargo, on_delete=models.CASCADE, related_name='cargo')
+
+	class Meta:
+		verbose_name = 'Perfil'
+		verbose_name_plural = 'Perfiles'
+		ordering = ['id']
+
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+	instance.profile.save()
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
