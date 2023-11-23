0c7a5919ee0f4a683ff4ab863309f55c9af36270
David Lima <david.limah@gmail.com>
2023-11-22 22:05:50 -0400

    Se trabaja sobre BD

--- correspondencia/models.py
+++ correspondencia/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Modelo Oficina
 class Oficina(models.Model):
     nombre_oficina = models.CharField(max_length=100)
@@ -24,7 +25,18 @@ class Cargo(models.Model):
         return self.nombre_cargo
 
 class Derivacion(models.Model):
-    pass
+    remitente = models.CharField(max_length=300)
+    
+    codigo_hoja_ruta = models.CharField(null=True, blank=True)
+    tipo_hoja_ruta = models.CharField(max_length=20)
+    fecha_derivado = models.DateTimeField()
+    asunto = models.CharField(max_length=300)
+
+
+    
 
 class Correspondencia(models.Model):
-    pass
+    numero_fojas = models.IntegerField()
+    fecha_enviado = models.DateTimeField()
+    fecha_recibido = models.DateTimeField(null=True, blank=True)
+    fecha_devuelto = models.DateTimeField(null=True, blank=True)
