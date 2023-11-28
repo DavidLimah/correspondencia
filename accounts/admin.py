a4fd127b2d4e5a93467c0f550ac3d920ca94aa88
David Lima <david.limah@gmail.com>
2023-11-28 18:05:02 -0400

    Se reestructura GUI

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Profile
 
 
+
 # Oficina, Cargo, 
 # Register your models here.
 # admin.site.register(Oficina)
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from correspondencia.views import add_group_name_to_context
 
 # 
 def home(request):
